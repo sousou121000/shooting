18d981cb90cfe4f2e16e7a9b8ff1ec38a46e65c9
sousou121000 <mhg-610-cmk@e-net.nara.jp>
2025-09-19 06:18:04 +0000

    javascriptの追加 ・自機の描画・移動 ・敵の追加

---
+++ game/main.js
@@ -0,0 +1,40 @@
+document.getElementById("txt").innerText="これはゲームです";
+const canvas=document.getElementById("gameCanvas");
+const ctx=canvas.getContext("2d");
+
+
+let x=225;
+let y=-50;
+let y1=0;
+
+window.addEventListener("keydown",(e)=>{
+    if(e.key==="ArrowLeft"){
+        x -= 10;
+    } else if(e.key==="ArrowRight"){
+        x += 10;
+    } else if(e.key === "Space"){
+      tama += 1;
+    }
+    });
+function gameLoop(){
+ctx.fillStyle="black";
+ctx.fillRect(0,0,canvas.width,canvas.height);
+
+ctx.fillStyle="blue";
+ctx.fillRect(x,500,30,30)
+
+ctx.fillStyle="yellow";
+ctx.fillRect(225,y1,30,30)
+y1 += 1;
+
+ctx.fillStyle="yellow";
+ctx.fillRect(100,y,30,30)
+y += 1;
+
+ctx.fillStyle="white";
+ctx.fillRect(x+10,400-tama*10,10,10);
+requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
+
