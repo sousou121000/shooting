0d66bbba10c589824f642df1ec52c89927cea7aa
sousou121000 <mhg-610-cmk@e-net.nara.jp>
2025-09-22 06:15:05 +0000

    球を撃つ

--- game/main.js
+++ game/main.js
@@ -1,38 +1,74 @@
-document.getElementById("txt").innerText="これはゲームです";
 const canvas=document.getElementById("gameCanvas");
 const ctx=canvas.getContext("2d");
 
+const player = {
+    x: canvas.width / 2 - 15,
+    y: canvas.height - 60,
+    width: 30,
+    height: 30,
+    color: "yellow",
+};
 
-let x=225;
-let y=-50;
-let y1=0;
+const bullets=[];
+const BULLET_SPEED= -5;
+
+function tryShoot(){
+    bullets.push({
+        x: player.x,
+        y: player.y,
+        width:5,
+        height:5,
+        vy:BULLET_SPEED,
+    })
+}
+
 
 window.addEventListener("keydown",(e)=>{
     if(e.key==="ArrowLeft"){
-        x -= 10;
+        if (player.x > 0){
+               player.x -= 10;     
+        }
+
     } else if(e.key==="ArrowRight"){
-        x += 10;
-    } else if(e.key === "Space"){
-     tama += 1;
+        if (player.x + player.width < canvas.width){
+            player.x += 10; 
+        }
+
+    } else if(e.code==="Space"){
+    tryShoot();
     }
     });
-function gameLoop(){
+
+function update(){
+    for(let i=0; i<bullets.length; i++){
+        const bullet=bullets[i];
+        bullet.y += bullet.vy;
+        if(bullet.y < 0){
+         bullets.splice(i,1);
+        }
+    }
+}
+
+function draw(){
 ctx.fillStyle="black";
 ctx.fillRect(0,0,canvas.width,canvas.height);
 
-ctx.fillStyle="blue";
-ctx.fillRect(x,500,30,30)
+ctx.fillStyle=player.color;
+ctx.fillRect(player.x, player.y, player.width, player.height);
 
-ctx.fillStyle="yellow";
-ctx.fillRect(225,y1,30,30)
-y1 += 1;
+ctx.fillStyle = "white";
+ for(let i=0; i<bullets.length; i++){
+    const bullet=bullets[i];
+    ctx.fillRect(bullet.x, bullet.y, bullet.width, bullet.height);
+ } 
+}
 
-ctx.fillStyle="yellow";
-ctx.fillRect(100,y,30,30)
-y += 1;
 
-ctx.fillStyle="white";
-ctx.fillRect(x+10,400-tama*10,10,10);
+function gameLoop(){
+
+update();
+draw();
+ 
 requestAnimationFrame(gameLoop);
 }
 
